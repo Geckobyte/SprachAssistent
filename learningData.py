fc5cb65b14d4ed086641a7b71b8cc45c139c013f
Geckobyte <geckobytedev@gmail.com>
2021-10-10 14:24:05 +0200

    Add files via upload

---
+++ learningData.py
@@ -0,0 +1,27 @@
+data = {}
+data[0] = [
+    "Wie spät ist es",
+    "Wie viel Uhr haben wir",
+    "Wie viel Uhr ist es",
+    "Wie spät",
+    "Uhrzeit",
+    "Welche Uhrzeit",
+    "Welche Uhrzeit haben wir",
+    "Wie spät haben wir es"
+]
+
+data[1] = [
+    "Was ist das heutige Datum",
+    "Welches Datum haben wir heute",
+    "Datum",
+    "Was für ein Datum haben wir"
+]
+
+data[2] = [
+    "Inzidenzzahl in Deutschland",
+    "Wie ist die Inzidenzzahl in Deutschland",
+    "Wie hoch ist die Inzidenzzahl hier",
+    "Inzidenzzahl",
+    "Wie ist der Inzidenzwert in Deutschland",
+    "Heutiger Inzidenzwert in Deutschland"
+]
